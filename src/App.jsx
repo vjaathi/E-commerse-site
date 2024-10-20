1cabf8f58ca82941501afcde984b08a3e29d210d
vjaathi <ponnuchamysam00@gmail.com>
2024-10-20 18:59:15 +0530

    modified cart

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter,Route, Routes } from "react-router-dom";
 import Shop from './pages/Shop';
 import ShopCategory from './pages/ShopCategory';
 import Product from './pages/Product';
-import Cart from './pages/cart';
+import Cart from './pages/Cart';
 import LoginSignup from './pages/LoginSignup';
 import Footer from "./components/footer/Footer";
 import men_banner from "./components/imges/banner_mens.png"
@@ -25,7 +25,7 @@ function App() {
             <Route path='/' element={<Shop/>}/>
             <Route path='/mens' element={<ShopCategory  banner={men_banner} category="men"/>}/>
             <Route path='/womens' element={<ShopCategory  banner={women_banner} category="women"/>}/>
-            <Route path='/kids' element={<ShopCategory  banner={kid_banner} category="kid~"/>}/>
+            <Route path='/kids' element={<ShopCategory  banner={kid_banner} category="kid"/>}/>
             <Route path='/product' element={<Product />}>
               <Route path=':productId' element={<Product />}/>
             </Route>
